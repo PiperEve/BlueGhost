ceed872a52069b748c3cfe708df111afe6c066e0
SophiaWestley <86091070+PiperEve@users.noreply.github.com>
2025-07-14 01:12:31 -0700

    babel.config.js and tsconfig.json updates

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
-  };
+    plugins: [
+		'nativewind/babel',
+  [
+    'module-resolver',
+    {
+      extensions: ['.tsx', '.ts', '.js', '.json'],
+      alias: {
+        '@components': './src/components',
+        '@screens': './src/screens',
+        '@assets': './assets',
+        '@utils': './src/utils',
+        '@styles': './src/styles'
+      }
+    }
+  ]
+]
+};
 };
