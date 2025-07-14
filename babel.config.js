934fba10f59f281a76a3ad4c0b5a08820ed9c604
SophiaWestley <86091070+PiperEve@users.noreply.github.com>
2025-07-14 01:29:35 -0700

    babel.config.js up

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,24 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-		'nativewind/babel',
-  [
-    'module-resolver',
-    {
-      extensions: ['.tsx', '.ts', '.js', '.json'],
-      alias: {
-        '@components': './src/components',
-        '@screens': './src/screens',
-        '@assets': './assets',
-        '@utils': './src/utils',
-        '@styles': './src/styles'
-      }
-    }
-  ]
-]
-};
+module.exports = function(api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			'nativewind/babel',
+			[
+				'module-resolver',
+				{
+					extensions: ['.tsx', '.ts', '.js', '.json'],
+					alias: {
+						'@components': './src /components',
+						'@screens': './src/screens',
+						'@api': './src/api',
+						'@navigation': './src/navigation',
+						'@state': './src/state',
+						'@types': './src/types',
+						'@utils': './src/utils'
+					}
+				}
+			]
+		]
+	};
 };
